5ca1d3245826d76dc5db515ffe96a4b3fb2202e7
aiaris <15129404526@163.com>
2022-11-30 12:32:25 +0800

    后台提交

---
+++ src/settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+  title: '后台管理系统',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  // 是否固定头部
+  fixedHeader: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  // 侧边栏logo是否显示
+  sidebarLogo: false
+}
